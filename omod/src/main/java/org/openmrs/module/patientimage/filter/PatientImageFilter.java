34fe6c3deb905eaf90ddbfc693dc77e65445fe5e
dkayiwa <kayiwadaniel@gmail.com>
2017-03-13 15:06:30 +0300

    Doing some file formatting

--- omod/src/main/java/org/openmrs/module/patientimage/filter/PatientImageFilter.java
+++ omod/src/main/java/org/openmrs/module/patientimage/filter/PatientImageFilter.java
@@ -7,6 +7,7 @@ package org.openmrs.module.patientimage.filter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -15,8 +16,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.openmrs.PatientIdentifier;
 
+import org.openmrs.PatientIdentifier;
 import org.openmrs.api.context.Context;
 
 /**
@@ -46,28 +47,23 @@ public class PatientImageFilter implements Filter {
 				StringBuilder servletResponse = new StringBuilder(responseWrapper.toString());
 				int indexOf = servletResponse.indexOf("<div class=\"patient-header \">");
 				String imgUrl;
-				if (Boolean.valueOf(Context.getAdministrationService().getGlobalProperty("patientimage.usePersonImageEndpoint"))) {
+				if (Boolean.valueOf(Context.getAdministrationService().getGlobalProperty(
+				    "patientimage.usePersonImageEndpoint"))) {
 					final String restEndpoint = "/ws/rest/v1/personimage";
-					imgUrl = request.getScheme() + "://"
-							+ request.getServerName() + ":"
-							+ request.getServerPort() + "/"
-							+ request.getContextPath()
-							+ restEndpoint + "/"
-							+ request.getParameter("patientId");
-				}
-				else {
+					imgUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/"
+					        + request.getContextPath() + restEndpoint + "/" + request.getParameter("patientId");
+				} else {
 					final String endpoint = "moduleServlet/patientimage";
 					final String servletName = "ImageServlet";
 					final String imageParamName = "image";
 					final String imageParamValue = id.getIdentifier() + ".jpg";
-
-					imgUrl = request.getContextPath() + "/"
-							+ endpoint + "/"
-							+ servletName + "?"
-							+ imageParamName + "="
-							+ imageParamValue;
+					
+					imgUrl = request.getContextPath() + "/" + endpoint + "/" + servletName + "?" + imageParamName + "="
+					        + imageParamValue;
 				}
-				String imgTag = "<img alt=\"\" id=\"imgThumbnail\" height=\"145\" src=\"" + imgUrl + "\" style=\"border: 1px solid #8FABC7; float:right\" onError=\"this.onerror = '';this.style.display='none';\">";
+				String imgTag = "<img alt=\"\" id=\"imgThumbnail\" height=\"145\" src=\""
+				        + imgUrl
+				        + "\" style=\"border: 1px solid #8FABC7; float:right\" onError=\"this.onerror = '';this.style.display='none';\">";
 				responseText = servletResponse.insert(indexOf, imgTag).toString();
 			}
 			out.write(responseText);
